285df2ca646d2a75bbce2ab0c65506c5a5a38fbb
yutongtcc <55568997+yutongtcc@users.noreply.github.com>
2019-10-18 09:36:49 +0800

    Add files via upload

--- light.cpp
+++ light.cpp
@@ -11,7 +11,14 @@ light::light()
 	getBrightness = 0;
 }
 
-light::light(std::string color)
+light::light(std::string aName)
+{
+	getColor="?";
+	getBrightness=0;
+	name=aName;
+}
+
+void light::set_color(std::string color)
 {
 	getColor=color;
 	
@@ -24,5 +31,14 @@ void light::set_brightness(int aBrightness)
 {
 	getBrightness=aBrightness;
 	std::cout<<" The light brightness is setted"<<getBrightness<<std::endl;
+}
+
+void light::onOff(bool state)
+{
+	cout<<"onOff called"<<endl;
+}
+
+vector<std::string> light::getFunctions(){
+	return functions;
 }
 
--- light.h
+++ light.h
@@ -3,16 +3,22 @@
 
 #include <iostream>
 #include <string>
+#include "appliance.h"
 
-class light
+class light: public appliance
 {
 public:
 	light();
-
-	light(std::string color);
+	light(std::string aName)
+	void set_color(std::string color);
 	std::string getColor;
 	void set_brightness(int aBrightness);
 	int getBrightness;
+
+	void onOff(bool state);
+
+	vector<std::string>getFunctions();
+	vector<std::string>functions;
 };
 
 
